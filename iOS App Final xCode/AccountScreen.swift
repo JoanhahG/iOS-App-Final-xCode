018a0dcede5930013310ac047049c6b4ebd7cf2c
JoanhahG <joanhahg@gmail.com>
2024-04-24 16:31:28 -0400

    Added Screens
    added all the view controller screens

--- iOS App Final xCode/AccountScreen.swift
+++ iOS App Final xCode/AccountScreen.swift
@@ -7,12 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AccountScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // Testing git
+        
     }
 //test 2
 
