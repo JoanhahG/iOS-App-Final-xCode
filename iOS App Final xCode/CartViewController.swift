6c1494184053b3ce34fc7728084c169e8216dd1b
Carr OConnor <coconnor@hydro-gear.com>
2024-04-28 19:56:05 -0400

    added itemcount

--- iOS App Final xCode/CartViewController.swift
+++ iOS App Final xCode/CartViewController.swift
@@ -11,5 +11,12 @@ class CartViewController: UIViewController {
     override func viewDidLoad() {
         //Cart controller code here
         self.title = "Cart"
+        var cart = [Product]()
+        //loop through current inventory and add items with an item count
+//        for int i = 0; i < Inventory.sharedInstance.currentInventory.count; i++:
+//            cart.append(i);
+        
     }
+    
+    //viewIsAppearing
 }
