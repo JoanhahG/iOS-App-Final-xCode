cf19320ad61c5e1e677685f62f51f5c836e3a1d8
JoanhahG <joanhahg@gmail.com>
2024-04-28 19:53:29 -0400

    random changes
    updated cart and in progress product card

--- iOS App Final xCode/CartViewController.swift
+++ iOS App Final xCode/CartViewController.swift
@@ -4,12 +4,29 @@
 //
 //  Created by Carr O'Connor on 4/24/24.
 //
-
 import UIKit
 
+var cartItems = [Product]()
+
 class CartViewController: UIViewController {
+    
+    @IBOutlet weak var CheckOutButton: UIButton!
+    
+    let inventory = Inventory.sharedInstance.currentInventory;
+   
+    override func viewDidAppear(_ animated: Bool) {
+        if cartItems.isEmpty {
+           // CheckOutButton.isEnabled = false
+           // print("hi")
+            
+        }
+  
+    }
+    
     override func viewDidLoad() {
         //Cart controller code here
         self.title = "Cart"
+        print(inventory[1])
     }
+    
 }
