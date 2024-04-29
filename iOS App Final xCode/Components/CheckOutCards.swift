022342ae69b13122e86ac04c38b89fb7a6435166
JoanhahG <joanhahg@gmail.com>
2024-04-28 23:16:12 -0400

    created productList
    I need this for testing my views it has to be global

--- iOS App Final xCode/Components/CheckOutCards.swift
+++ iOS App Final xCode/Components/CheckOutCards.swift
@@ -6,10 +6,10 @@
 //
 
 import SwiftUI
-
+let productList = [Product(id: 1, title: "Oranges", price: 3.99, category: "fruit", cartCount: 0, invCount: 99, description: "Fresh and Orange", image: "orange"),Product(id: 2, title: "pears", price: 3.99, category: "fruit", cartCount: 0, invCount: 99, description: "Fresh and Orange", image: "orange"), Product(id: 3, title: "grapes", price: 3.99, category: "fruit", cartCount: 0, invCount: 99, description: "Fresh and Orange", image: "orange"),Product(id: 4, title: "watermelon", price: 3.99, category: "fruit", cartCount: 0, invCount: 99, description: "Fresh and Orange", image: "orange"),Product(id: 5, title: "hungry", price: 3.99, category: "fruit", cartCount: 0, invCount: 99, description: "Fresh and Orange", image: "orange")]
 
 struct CheckOutCards: View {
-    let product = Product(id: 1, title: "Oranges", price: 3.99, category: "fruit", cartCount: 0, invCount: 99, description: "Fresh and Orange", image: "orange")
+        var product: Product
     var body: some View {
         ZStack{
             Image("beige").resizable().frame(width: 390, height: 200).cornerRadius(25)
@@ -41,7 +41,7 @@ struct CheckOutCards: View {
     }
 }
 #Preview{
-        CheckOutCards()
+        CheckOutCards(product: productList[2])
 }
 
 
