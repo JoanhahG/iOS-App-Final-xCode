fb226a8b456231661dc2ff9d066cf6385133cca4
Carr OConnor <coconnor@hydro-gear.com>
2024-04-29 00:39:54 -0400

    Home page collectionview

---
+++ iOS App Final xCode/HomeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HomeCollectionViewCell.swift
+//  iOS App Final xCode
+//
+//  Created by Carr O'Connor on 4/29/24.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
+}
--- iOS App Final xCode/HomeViewController.swift
+++ iOS App Final xCode/HomeViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         //Home controller code here
         self.title = "Home"
         
         var inventory = Inventory.sharedInstance.currentInventory;
         print(inventory)
+        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+    }
+}
+
+extension HomeViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! HomeCollectionViewCell
+        cell.productTitle.text = Inventory.sharedInstance.currentInventory[indexPath.row].title
+        cell.productImage.image = UIImage(named: "bananas1")
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Inventory.sharedInstance.currentInventory.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = (collectionView.frame.size.width-10)/2
+        return CGSize(width: size, height: size)
     }
 }
