e6c1326893de6140bdb4c481f474a4a533dff760
Carr OConnor <coconnor@hydro-gear.com>
2024-05-02 14:56:18 -0400

    organization, performance improvement

--- iOS App Final xCode/HomeCollectionViewCell.swift
+++ iOS App Final xCode/HomeCollectionViewCell.swift
@@ -20,7 +20,23 @@ class HomeCollectionViewCell: UICollectionViewCell {
             // Customize stepper icon colors
             itemCountStepper.setDecrementImage(itemCountStepper.decrementImage(for: .normal), for: .normal)
             itemCountStepper.setIncrementImage(itemCountStepper.incrementImage(for: .normal), for: .normal)
-            itemCountStepper.tintColor = UIColor.systemGray5 // Change color to your desired color
+            itemCountStepper.tintColor = UIColor.black // Change color to your desired color
+        }
+    
+    override func layoutSubviews() {
+            super.layoutSubviews()
+            
+            self.contentView.layer.cornerRadius = 8.0 // Adjust as needed
+            self.contentView.layer.masksToBounds = true
+            self.contentView.layer.borderWidth = 1.0
+            self.contentView.layer.borderColor = UIColor.clear.cgColor
+            self.layer.shadowColor = UIColor.systemCyan.cgColor
+            self.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+            self.layer.shadowRadius = 2.0
+            self.layer.shadowOpacity = 0.5
+            self.layer.masksToBounds = false
+            self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.contentView.layer.cornerRadius).cgPath
+        
         }
     
     @IBAction func StepperValueChanged(_ sender: UIStepper) {
