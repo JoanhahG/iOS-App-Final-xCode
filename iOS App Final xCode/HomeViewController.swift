d51ffa7d5bd1180a46d45526ec26e0a7af918571
Carr OConnor <coconnor@hydro-gear.com>
2024-05-02 14:46:59 -0400

    some styling

--- iOS App Final xCode/HomeViewController.swift
+++ iOS App Final xCode/HomeViewController.swift
@@ -14,6 +14,9 @@ class HomeViewController: UIViewController {
         self.title = "Store"
         
         collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        let padding: CGFloat = 5
+        collectionView.contentInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+
     }
 }
 
@@ -21,12 +24,39 @@ extension HomeViewController : UICollectionViewDelegate, UICollectionViewDataSou
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! HomeCollectionViewCell
         let item = Inventory.sharedInstance.currentInventory[indexPath.row]
+        var size = 100.00
+        if UIDevice.current.orientation.isPortrait{
+            size = ((collectionView.frame.size.width-10)/2.2) - 50
+        }
+        else{
+            size = ((collectionView.frame.size.height-10)/2.2) - 50
+        }
+        
+        cell.contentView.layer.cornerRadius = 2.0
+        cell.contentView.layer.borderWidth = 1.0
+        cell.contentView.layer.borderColor = UIColor.clear.cgColor
+        cell.contentView.layer.masksToBounds = true
+
+        cell.layer.shadowColor = UIColor.systemCyan.cgColor
+        cell.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        cell.layer.shadowRadius = 2.0
+        cell.layer.shadowOpacity = 0.5
+        cell.layer.masksToBounds = false
+        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
+        
         cell.productTitle.text = item.title
         cell.productImage.image = UIImage(named: item.image)
+        cell.productImage.widthAnchor.constraint(equalToConstant: CGFloat(size)).isActive = true
+        cell.productImage.heightAnchor.constraint(equalToConstant: CGFloat(size)).isActive = true
         cell.itemCountLabel.text = String(item.cartCount)
         cell.priceLabel.text = item.price.formatted(.currency(code: "USD"))
+        cell.itemCountStepper.setDecrementImage(UIImage(systemName: "minus"), for: .normal)
+        cell.itemCountStepper.setIncrementImage(UIImage(systemName: "plus"), for: .normal)
+        cell.itemCountStepper.tintColor = UIColor.black
         cell.itemCountStepper.value = Double(item.cartCount)
         cell.itemCountStepper.tag = indexPath.row
+        cell.contentView.layer.cornerRadius = 2.0
+        cell.contentView.layer.backgroundColor = CGColor(gray: 5, alpha: 0)
         return cell
         
     }
@@ -38,12 +68,12 @@ extension HomeViewController : UICollectionViewDelegate, UICollectionViewDataSou
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         if UIDevice.current.orientation.isPortrait{
-            let size = (collectionView.frame.size.width-10)/2
-            return CGSize(width: size, height: size)
+            let size = (collectionView.frame.size.width-10)/2.2
+            return CGSize(width: size, height: size + (size * 0.2))
         }
         else {
-            let size = (collectionView.frame.size.height)/2
-            return CGSize(width: size+20, height: size+20)
+            let size = (collectionView.frame.size.height)/2.2
+            return CGSize(width: size + (size * 0.4), height: size + (size * 0.2))
         }
     }
 }
